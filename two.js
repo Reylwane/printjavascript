6b8ac8bf466348b637f2d60d0fc62567c4d1ed55
Reylwane <107673099+Reylwane@users.noreply.github.com>
2022-06-24 05:29:43 -0700

    Add files via upload

---
+++ two.js
@@ -0,0 +1,12 @@
+let firstName = "ELELU", 
+    lastName = "RIDWAN",
+    height = "9ft",
+    country = "Nigeria",
+
+    fullName = firstName + ' ' + lastName;
+
+console.log(fullName);
+console.log(height);
+console.log(country);
+
+alert(`${fullName} \n${height} \n${country}`)
